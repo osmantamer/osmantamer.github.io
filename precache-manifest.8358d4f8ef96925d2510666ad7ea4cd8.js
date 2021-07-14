6ba6e5623c96f67aabb3fd88302f9a06661c0082
osman <osmntamer@gmail.com>
2021-07-14 12:06:00 +0300

    blogs added

---
+++ precache-manifest.8358d4f8ef96925d2510666ad7ea4cd8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7dc4df7ff06cfba515",
+    "url": "/css/app.0d206cf0.css"
+  },
+  {
+    "revision": "45f94e4a5d86a7922435",
+    "url": "/css/chunk-vendors.5eb23405.css"
+  },
+  {
+    "revision": "ea5475e576be6c73eef96dc3fdaf6186",
+    "url": "/img/arrow_blog1/read_int_py_to_cpp.png"
+  },
+  {
+    "revision": "0811f66c9324a1689acdf752a2edd845",
+    "url": "/img/arrow_blog1/total_int_py_to_cpp.png"
+  },
+  {
+    "revision": "9c24ce3c59498983699e2b704c914b84",
+    "url": "/img/arrow_blog1/write_int_py_to_cpp.png"
+  },
+  {
+    "revision": "7883dc237c4f220447e47d8909c6d0c9",
+    "url": "/img/blogs/vue.jpeg"
+  },
+  {
+    "revision": "a6ff72d9d258191713a456ff3f1189c8",
+    "url": "/img/certifications/IBM_data_analysis_python.png"
+  },
+  {
+    "revision": "276284689cf5a40bee426d18f45d7dae",
+    "url": "/img/certifications/google.png"
+  },
+  {
+    "revision": "cc0023ab225d61d85f25ab03afee2a4b",
+    "url": "/img/profile.cc0023ab.png"
+  },
+  {
+    "revision": "513324514006c2d4ff2f7f68db8c319a",
+    "url": "/img/projects/karakoygourmet.png"
+  },
+  {
+    "revision": "4bd7e9ac5ad2d003d5700f1e5a124e81",
+    "url": "/img/workExperience/arrow.png"
+  },
+  {
+    "revision": "77b8d4301d60c0ebcee2d7c373cc1304",
+    "url": "/img/workExperience/bilkav.png"
+  },
+  {
+    "revision": "899280c7ef936fe17f8587790833f6c2",
+    "url": "/img/workExperience/delft.png"
+  },
+  {
+    "revision": "4873132decc57ac01afcbdaa7ae3ad32",
+    "url": "/img/workExperience/freelance.png"
+  },
+  {
+    "revision": "5e24f3aa6a079ffd44693c9d8acbcc21",
+    "url": "/img/workExperience/optiwisdom.png"
+  },
+  {
+    "revision": "4e0a134311117585e1a8089412b20f9c",
+    "url": "/img/workExperience/sehir.png"
+  },
+  {
+    "revision": "5e6191fd20378045f3c99cc76d2ff1ff",
+    "url": "/img/workExperience/tubitak.png"
+  },
+  {
+    "revision": "1d7a47ab6ffb847a9adac6e8f6e827b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c7dc4df7ff06cfba515",
+    "url": "/js/app.0e4f7b64.js"
+  },
+  {
+    "revision": "45f94e4a5d86a7922435",
+    "url": "/js/chunk-vendors.98e7a6c2.js"
+  },
+  {
+    "revision": "d2bdea41a961875c8677671c242f1d62",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
