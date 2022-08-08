e1b14c6ce59e61d606f328119e8fd78d983b9215
osmantamer <osmntamer@gmail.com>
2022-08-08 17:46:43 +0300

    portfolio updated

---
+++ precache-manifest.ba81a1392684a5208a329f99b1612994.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1356bb6f3717b25dfe",
+    "url": "/css/app.0d206cf0.css"
+  },
+  {
+    "revision": "185d4781252db8801f09",
+    "url": "/css/chunk-vendors.17bd09f4.css"
+  },
+  {
+    "revision": "ea5475e576be6c73eef96dc3fdaf6186",
+    "url": "/img/arrow_blog1/read_int_py_to_cpp.png"
+  },
+  {
+    "revision": "0811f66c9324a1689acdf752a2edd845",
+    "url": "/img/arrow_blog1/total_int_py_to_cpp.png"
+  },
+  {
+    "revision": "9c24ce3c59498983699e2b704c914b84",
+    "url": "/img/arrow_blog1/write_int_py_to_cpp.png"
+  },
+  {
+    "revision": "7883dc237c4f220447e47d8909c6d0c9",
+    "url": "/img/blogs/vue.jpeg"
+  },
+  {
+    "revision": "a6ff72d9d258191713a456ff3f1189c8",
+    "url": "/img/certifications/IBM_data_analysis_python.png"
+  },
+  {
+    "revision": "276284689cf5a40bee426d18f45d7dae",
+    "url": "/img/certifications/google.png"
+  },
+  {
+    "revision": "d5274f615458cad2fb5030c1dcca46a1",
+    "url": "/img/profile.d5274f61.png"
+  },
+  {
+    "revision": "513324514006c2d4ff2f7f68db8c319a",
+    "url": "/img/projects/karakoygourmet.png"
+  },
+  {
+    "revision": "355275afa1f08a5f7b20cce50063f1ee",
+    "url": "/img/projects/narlink.png"
+  },
+  {
+    "revision": "4bd7e9ac5ad2d003d5700f1e5a124e81",
+    "url": "/img/workExperience/arrow.png"
+  },
+  {
+    "revision": "77b8d4301d60c0ebcee2d7c373cc1304",
+    "url": "/img/workExperience/bilkav.png"
+  },
+  {
+    "revision": "0bf08e9be5b41a437ccb37e7f76e2156",
+    "url": "/img/workExperience/deepinfo.png"
+  },
+  {
+    "revision": "899280c7ef936fe17f8587790833f6c2",
+    "url": "/img/workExperience/delft.png"
+  },
+  {
+    "revision": "4873132decc57ac01afcbdaa7ae3ad32",
+    "url": "/img/workExperience/freelance.png"
+  },
+  {
+    "revision": "5e24f3aa6a079ffd44693c9d8acbcc21",
+    "url": "/img/workExperience/optiwisdom.png"
+  },
+  {
+    "revision": "4e0a134311117585e1a8089412b20f9c",
+    "url": "/img/workExperience/sehir.png"
+  },
+  {
+    "revision": "5e6191fd20378045f3c99cc76d2ff1ff",
+    "url": "/img/workExperience/tubitak.png"
+  },
+  {
+    "revision": "cf0024754aa304ae2c4d996349ec798b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e1356bb6f3717b25dfe",
+    "url": "/js/app.e1a09af8.js"
+  },
+  {
+    "revision": "185d4781252db8801f09",
+    "url": "/js/chunk-vendors.b6a9af49.js"
+  },
+  {
+    "revision": "d2bdea41a961875c8677671c242f1d62",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
